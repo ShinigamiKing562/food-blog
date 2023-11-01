bd1aef99026af7bffb8127de20e1ab8ea3c99b00
ShinigamiKing562 <vkamau562@gmail.com>
2023-11-01 21:51:57 +0300

    separated the websiteinto different webpages

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function changeBackground() {
-  var scrollPosition = window.scrollY;
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const mainContent = document.querySelector("main");
 
-  if (scrollPosition < 600) {
-    document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/ShinigamiKing562/food-blog/main/Multimedia%20files/back1.jpg)";
-  } else if (scrollPosition < 1000) {
-    document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/ShinigamiKing562/food-blog/main/Multimedia%20files/back2.jpg)";
-  } else {
-    document.body.style.backgroundImage = "url(https://raw.githubusercontent.com/ShinigamiKing562/food-blog/main/Multimedia%20files/back3.jpg)";
-  }
-}
-
-window.addEventListener("scroll", changeBackground);
+  toggleButton.addEventListener("click", function () {
+      mainContent.innerHTML = ""; // Clear the main content
+      mainContent.innerHTML = '<object data="main.html"></object>'; // Load the main content
+  });
+});
