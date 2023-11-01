63c339555eb02528efe017ded206ed3c6f2147f2
ShinigamiKing562 <vkamau562@gmail.com>
2023-11-01 23:01:37 +0300

    added another webpage

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggleButton");
-  const mainContent = document.querySelector("main");
-
-  toggleButton.addEventListener("click", function () {
-      mainContent.innerHTML = ""; // Clear the main content
-      mainContent.innerHTML = '<object data="main.html"></object>'; // Load the main content
-  });
-});
